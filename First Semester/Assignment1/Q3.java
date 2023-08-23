0496a58a6c14f091cd3396357f445a1859e1b74f
SOMNATH0904 <130086832+SOMNATH0904@users.noreply.github.com>
2023-08-24 00:45:52 +0530

    Add files via upload

---
+++ First Semester/Assignment1/Q3.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+public class Q3 {
+
+    public static void main(String[] args) {
+        System.out.print("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # \n");
+        System.out.print("# #                                                           # # \n");
+        System.out.print("# #                                                           # # \n");
+        System.out.print("# #                                                           # # \n");
+        System.out.print("# #                                                           # # \n");
+        System.out.print("# #                                                           # # \n");
+        System.out.print("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # \n");
+
+    }
+
+}
---
+++ First Semester/Assignment1/Q9.java
@@ -0,0 +1,12 @@
+package Assignment1;
+
+public class Q9 {
+    public static void main(String[] args) {
+        System.out.println('5');
+        System.out.println((float)25/6);
+        System.out.println(5+'6');
+        System.out.println(5+7+'9');
+        System.out.println("92"+7+5);
+        System.out.println(2+"9");
+    }
+}
