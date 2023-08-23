9a4b7f3af9a035cc72130bfa9ad029091d2e4034
SOMNATH0904 <130086832+SOMNATH0904@users.noreply.github.com>
2023-08-24 00:49:27 +0530

    Add files via upload

---
+++ First Semester/Assignment2/Q7.java
@@ -0,0 +1,21 @@
+package Assignment2;
+
+public class Q7 {
+    public static void main(String[] args) {
+        int a=1, b=2;
+        System.out.println("a \t b \t pow(a,b)");
+        System.out.println(a +"\t" + b +"\t" +  (int) Math.pow(a, b));
+        a= a+1;
+        b= b+1;
+        System.out.println(a +"\t" + b +"\t" +  (int) Math.pow(a, b));
+        a= a+1;
+        b= b+1;
+        System.out.println(a +"\t" + b +"\t" +  (int) Math.pow(a, b));
+        a= a+1;
+        b= b+1;
+        System.out.println(a +"\t" + b +"\t" +  (int) Math.pow(a, b));
+        a= a+1;
+        b= b+1;
+        System.out.println(a +"\t" + b +"\t" +  (int) Math.pow(a, b));
+    }
+}
