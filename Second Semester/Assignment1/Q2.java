78880c494eda8187c3b12754950c913332560fb4
SOMNATH0904 <130086832+SOMNATH0904@users.noreply.github.com>
2023-08-29 10:59:07 +0530

    Add files via upload

---
+++ Second Semester/Assignment1/Q2.java
@@ -0,0 +1,10 @@
+package Assignment1;
+
+public class Q2 {
+    public static void main(String[] args) {
+        System.out.println("*******     *******");
+        System.out.println("      *     *     *");
+        System.out.println("      *******     ******* ");
+
+    }
+}
