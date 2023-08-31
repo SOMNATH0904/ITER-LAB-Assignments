3b34e587a5c79c4c1b96b2856ea8bbf865e2ac46
SOMNATH0904 <130086832+SOMNATH0904@users.noreply.github.com>
2023-08-31 09:31:59 +0530

    Delete Second Semester/Assignment1 directory

--- Second Semester/Assignment1/Q4.java
+++
@@ -1,7 +0,0 @@
-package Assignment1;
-
-public class Q4 {
-    public static void main(String[] args) {
-        System.out.print("My First Name is : Somnath \nMiddle Name: \nLast Name : Shaw");
-    }
-}
