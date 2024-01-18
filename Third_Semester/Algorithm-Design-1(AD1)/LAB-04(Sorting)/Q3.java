45f65ddc030d1bc5fcc322938c9babeecd6ad11f
SOMNATH0904 <somnathshaw900@gmail.com>
2024-01-18 22:45:07 +0530

    Updated AD Lab Assignment

--- Third_Semester/Algorithm-Design-1(AD1)/LAB-04(Sorting)/Q3.java
+++ Third_Semester/Algorithm-Design-1(AD1)/LAB-04(Sorting)/Q3.java
@@ -1,46 +1,48 @@
 // Ques 3 : Check reverse
 
-import java.util.Arrays;
-
 class Q3 {
-    public static boolean checkReverse(int arr[], int n) {
-        int temp[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            temp[i] = arr[i];
-        }
-        Arrays.sort(temp);
-        int front;
-        for(front = 0; front < n; front++) {
-            if(temp[front] != arr[front]) {
-                break;
-            }
-        }
-        int back;
-        for(back = n-1; back >= 0; back--) {
-            if(temp[back] != arr[back]) {
-                break;
-            }
-        }
-        if(front >= back) {
-            return true;
-        }
-        do{
-            front++;
-            if (arr[front - 1] < arr[front]) {
-                return false;
-            }
-        } while(front != back);
-        return true;
-    }
 
-    public static void main(String[] args) {
-        int arr[] = {1, 2, 5, 4, 3};
-        int n = arr.length;
-        System.out.print("Is the Array Sorted : ");
-        if (checkReverse(arr, n)) {
-            System.out.print("True");
-        } else {
-            System.out.print("False");
-        }
-    }
-}
+	static boolean checkReverse(int arr[], int n) { 
+		if (n == 1) { 
+			return true; 
+		}
+		int i; 
+		for (i = 1; arr[i - 1] < arr[i] && i < n; i++); 
+		if (i == n) { 
+			return true; 
+		}
+		int j = i; 
+		while (j < n && arr[j] < arr[j - 1]) { 
+			if (i > 1 && arr[j] < arr[i - 2]) { 
+				return false; 
+			} 
+			j++; 
+		}
+		if (j == n) return true; 
+		int k = j;
+		if (arr[k] < arr[i - 1]) { 
+			return false; 
+		} 
+
+		while (k > 1 && k < n) { 
+			if (arr[k] < arr[k - 1]) { 
+				return false; 
+			} 
+			k++; 
+		} 
+		return true; 
+	} 
+
+	public static void main(String[] args) { 
+
+		int arr[] = {1, 3, 4, 10, 9, 8}; 
+		int n = arr.length; 
+
+		if (checkReverse(arr, n)) { 
+			System.out.print("True"); 
+		} else { 
+			System.out.print("False"); 
+		} 
+	} 
+	} 
+} 
