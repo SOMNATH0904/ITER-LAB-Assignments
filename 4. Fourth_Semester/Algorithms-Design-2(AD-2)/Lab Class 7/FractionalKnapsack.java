c0c6d81a2bbe6d9289e6bd888d0d3b4d8027ce46
SOMNATH0904 <somnathshaw900@gmail.com>
2024-04-04 17:29:26 +0530

    Updated the AD2 Lab Class 7 record

--- 4. Fourth_Semester/Algorithms-Design-2(AD-2)/Lab Class 7/FractionalKnapsack.java
+++ 4. Fourth_Semester/Algorithms-Design-2(AD-2)/Lab Class 7/FractionalKnapsack.java
@@ -5,13 +5,77 @@
 /**
  * ALGORITHM
  * 
- * 
+ * function FractionalKnapsack ( w [],c[],wk) 
+ *      for i = 1 to n do 
+ *          x[i] = 0
+ *      Weight = 0
+ *      H = BuildMaxHeap (C/W)      //Use Max heap 
+ *      while Weight < Wk do 
+ *          i = H H.GetMax() 
+ *          if(weight+ W[i] <= Wk ) do 
+ *              X[i] = 1
+ *              weight = weight+ w[i]
+ *          else
+ *              x[i] =(Wk-weight)/w[i]
+ *              Weight = Wk
+ *       return X
  */
 
+/**
+ * CODE
+ */
+
+import java.util.Arrays;
+
+public class FractionalKnapsack {
+    private class Items implements Comparable<Items> {
+        int wt;
+        int cost;
+        double density;
+
+        Items(int w, int v) {
+            wt =w;
+            cost =v;
+            density = (double) cost / wt;
+        }
+
+        public int compareTo(Items s2) { // decreasing order
+            return (int) (s2.density - this.density);
+        }
+    }
+
+    public double getMaxCostFractional(int[] wt, int[] cost, int capacity) {
+        double totalCost = 0;
+        int n = wt.length;
+        Items[] itemList = new Items[n];
+        for(int i = 0; i < n; i++)
+            itemList[i] = new Items(wt[i], cost[i]);
+        Arrays.sort(itemList);
+        for(int i = 0; i < n; i++) {
+            if (capacity - itemList[i].wt >= 0) {
+                capacity -= itemList[i].wt;
+                totalCost += itemList[i].cost;
+            } else {
+                totalCost += (itemList[i].density * capacity);
+                break;
+            }
+        }
+        return totalCost;
+    }
+
+    public static void main(String[] args) {
+        int[] wt = { 10, 40, 20, 30 };
+        int[] cost = { 60, 40, 90, 120 };
+        int capacity = 50;
+        FractionalKnapsack kp = new FractionalKnapsack();
+        double maxCost = kp.getMaxCostFractional(wt, cost, capacity);
+        System.out.println("Maximum cost obtained : "+maxCost);
+    }
+}
 
 
 /**
  * OUTPUT
  * 
- * 
+ * Maximum cost obtained : 230.0
  */
--- 4. Fourth_Semester/Algorithms-Design-2(AD-2)/Lab Class 7/HuffmanCoding.java
+++ 4. Fourth_Semester/Algorithms-Design-2(AD-2)/Lab Class 7/HuffmanCoding.java
@@ -5,13 +5,103 @@
 /**
  * ALGORITHM
  * 
+ * // Computes optimal prefix code.
+ * // Input: List W of character probabilities
+ * // Output: The Huffman tree.
  * 
+ * function Huffman(C[0..n-1], W[0..n-1]) 
+ *      PQ = {}  // priority queue
+ *      for i = 0 to n-1 do 
+ *          T.char = C[i] 
+ *          T.weight = w[i] 
+ *          add T to priority queue PQ
+ * 
+ *      for i = 0 to n-2 do
+ *          L = remove min from PQ
+ *          R = remove min from PQ
+ *          T = node with children L and R 
+ *          T.weight = L.weight + R.weight \
+ *          add T to priority queue PQ
+ *      return T
  */
 
+/**
+ * CODE
+ */
 
+import java.util.PriorityQueue;
+
+public class HuffmanCoding {
+    Node root = null;
+
+    class Node implements Comparable<Node> {
+        char c;
+        int freq;
+        Node left;
+        Node right;
+
+        Node(char ch, int fr, Node l, Node r) {
+            c = ch;
+            freq = fr;
+            left = l;
+            right = r;
+        }
+
+        public int compareTo(Node n2) {
+            return this.freq - n2.freq;
+        }
+    }
+
+    HuffmanCoding(char[] arr, int[] freq) {
+        int n = arr.length;
+        PriorityQueue<Node> que = new PriorityQueue<Node>(n);
+
+        for(int i = 0; i < n; i++) {
+            Node node = new Node(arr[i], freq[i], null, null);
+            que.add(node);
+        }
+
+        while(que.size() > 1) {
+            Node lt = que.peek();
+            que.poll();
+            Node rt = que.peek();
+            que.poll();
+
+            Node nd = new Node('+', lt.freq + rt.freq, lt, rt);
+            que.add(nd);
+        }
+        root = que.peek();
+    }
+
+    private void print(Node root, String s) {
+        if (root.left == null && root.right == null && root.c != '+') {
+            System.out.println(root.c + " : " + s);
+            return;
+        }
+        print(root.left,s + "0");
+        print(root.right, s + "1") ;
+    }
+
+    public void print() {
+        System.out.println("Char = Huffman code");
+        print(root, "");
+    }
+
+    public static void main(String[] args) {
+        char[] ar = { 'A', 'B', 'C', 'D', 'E' };
+        int[] fr = { 30, 25, 21, 14, 10 };
+        HuffmanCoding hf = new HuffmanCoding(ar, fr);
+        hf.print();
+    }
+}
 
 /**
  * OUTPUT
  * 
- * 
+ * Char = Huffman code
+ * C : 00
+ * E : 010
+ * D : 011
+ * B : 10
+ * A : 11
  */
