3c5f5a43c028132899f43692361f276846021d93
SOMNATH0904 <somnathshaw900@gmail.com>
2024-11-17 17:15:07 +0530

    Python : Corrected the Code

--- 5. Fifth_Semester/PYTHON/Minor Assignment 2/Q07.py
+++ 5. Fifth_Semester/PYTHON/Minor Assignment 2/Q07.py
@@ -3,7 +3,7 @@ sumPrimes = 0
 
 for i in range(2, rangePrime):
     isPrime = True
-    for n in range(2, n):
+    for n in range(2, int(i**0.5)+1):
         if i % n == 0:
             isPrime = False
             break
