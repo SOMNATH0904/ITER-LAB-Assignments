3a4dcd815d34d1c88a8cc210d9dd27af71bf095b
SOMNATH0904 <somnathshaw900@gmail.com>
2024-11-18 17:32:58 +0530

    Python : Corrected the Code

---
+++ 5. Fifth_Semester/PYTHON/Minor Assignment 4/Q05.py
@@ -0,0 +1,25 @@
+'''
+Ques 5: Are there any mistakes in these codes? State the outputs:
+a) day, high temperature = (’Monday’, 87, 65)
+b) numbers = [1, 2, 3, 4, 5]
+numbers[10]
+c) name = ’amanda’
+name[0] = ’A’
+d) numbers = [1, 2, 3, 4, 5]
+numbers[3.4]
+e) student tuple = (’Amanda’, ’Blue’, [98, 75, 87])
+student tuple[0] = ’Ariana’
+f) (’Monday’, 87, 65) + ’Tuesday’
+g) ’A’ += (’B’, ’C’)
+h) x = 7
+del x
+print(x)
+i) numbers = [1, 2, 3, 4, 5]
+numbers.index(10)
+j) numbers = [1, 2, 3, 4, 5]
+numbers.extend(6, 7, 8)
+k) numbers = [1, 2, 3, 4, 5]
+numbers.remove(10)
+l) values = []
+values.pop()
+'''
---
+++ 5. Fifth_Semester/PYTHON/Minor Assignment 4/Q09.py
@@ -0,0 +1,33 @@
+'''
+Ques 9: Given the following inputs, indicate in each case (1) to (16), whether the statements will execute
+successfully. If, so, give what will be the outcome of execution? Also give the output of print
+statements (where applicable):
+address = ’B-6, Lodhi road, Delhi’
+list1 = [1, 2, 3]
+list2 = [’a’, 1, ’z’, 26, ’d’, 4]
+tuple1 = (’a’, ’e’, ’i’, ’o’, ’u’)
+tuple2 = ([2,4,6,8], [3,6,9], [4,8], 5)
+1. list1[3] = 4
+2. print(list1 * 2)
+3. print(min(list2))
+4. print(max(list1))
+5. print(list(address))
+6. list2.extend([’e’, 5])
+print(list2)
+7. list2.append([’e’, 5])
+print(list2)
+8. names = [’rohan’, ’mohan’, ’gita’]
+names.sort(key= len)
+print(names)
+9. list3 = [(x * 2) for x in range(1, 11)]
+print(list3)
+10. del list3[1:]
+print(list3)
+11. list4 = [ x+y for x in range(1,5) for y in range(1,5)]
+print(list4)
+12. tuple2[3] = 6
+13. tuple2.append(5)
+14. t1 = tuple2 +(5)
+15. ’ , ’ .join(tuple1)
+16. list(zip([’apple’, ’orange’], (’red’,’orange’)))
+'''
