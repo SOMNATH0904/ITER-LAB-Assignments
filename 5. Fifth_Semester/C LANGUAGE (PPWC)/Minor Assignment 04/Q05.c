6e9433d20658fedf022d2f33582809d3d235243a
Somnath Shaw <130086832+SOMNATH0904@users.noreply.github.com>
2024-11-21 22:24:33 +0530

    PPWC : Corrected the Code

--- 5. Fifth_Semester/C LANGUAGE (PPWC)/Minor Assignment 04/Q05.c
+++ 5. Fifth_Semester/C LANGUAGE (PPWC)/Minor Assignment 04/Q05.c
@@ -2,11 +2,11 @@
 
 int main() {
     int a = 12, b = 25, c = 18;
-    int *ptr = &a;
-
-    for (int i = 0; i < 3; i++) {
-        *(ptr + i) += 10;
-    }
+    int *ptrA = &a, *ptrB = &b, *ptrC = &c;
+    
+    *ptrA += 10;
+    *ptrB += 10;
+    *ptrC += 10;
 
     printf("Updated values:\n a: %d\n b: %d\n c: %d\n", a, b, c);
     return 0;
