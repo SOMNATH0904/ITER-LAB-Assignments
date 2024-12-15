4775534d2690c007054bb019b2e436675ead7971
SOMNATH0904 <somnathshaw900@gmail.com>
2024-12-15 23:18:27 +0530

    Python : Updated Minor Assignment 05

---
+++ 5. Fifth_Semester/PYTHON/Minor Assignment 5/Q04.py
@@ -0,0 +1,12 @@
+'''
+Ques 4. Make an English-to-French dictionary called e2f and print it. Here are your starter words: dog is
+chien, cat is chat, and walrus is morse.
+'''
+
+e2f = {"dog": "chien", "cat": "chat", "walrus": "morse"}
+print(e2f)
+
+'''
+Output ->
+{'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
+'''
