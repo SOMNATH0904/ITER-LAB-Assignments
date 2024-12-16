e2c3e4897c9d608face4267cfb66126e1e32eed9
SOMNATH0904 <somnathshaw900@gmail.com>
2024-12-16 19:49:51 +0530

    Python: Updated Minor Assignment 06

---
+++ 5. Fifth_Semester/PYTHON/Minor Assignment 6/Q01.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array_ones = np.ones((2, 3))
+print(array_ones)
+
+array_zeros = np.zeros((3, 3))
+print(array_zeros)
+
+array_sevens = np.full((2, 5), 7)
+print(array_sevens)
